b224cd1ca13c4627af3567466eb2eb3386cb7686
sjquant <seonujang92@gmail.com>
2019-11-12 01:47:36 +0900

    work on profile-edit-item css and js

--- components/my-page/ProfileEditItem.vue
+++ components/my-page/ProfileEditItem.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="profile-edit-item">
-    <div v-if="!editable">
+  <div class="profile-edit-item" :class="{editable: editable}">
+    <div v-show="!editable">
       <label>{{ label }}</label>
       <div class="value-container">
         <span>{{ value }}</span>
-        <button @click.stop>수정</button>
+        <button @click="editItem">수정</button>
       </div>
     </div>
-    <div v-else>
+    <div v-show="editable">
       <label>{{ label }}</label>
-      <input
-        id="email-fieled"
-        v-validate="'required'"
-        type="email"
-        name="이메일"
-        placeholder="example@naver.com"
-        :value="value"
-      />
-      <div
-        class="login-error-message"
-        v-show="errors.collect('이메일').length > 0"
-      >{{ errors.first('이메일') }}</div>
+      <div class="value-container">
+        <div>
+          <input
+            v-validate="inputValidate"
+            :type="inputType"
+            :name="label"
+            :placeholder="placeholder"
+            :value="value"
+            ref="itemInput"
+          />
+          <div
+            class="profile-edit-error-message"
+            v-show="errors.collect(label).length > 0"
+          >{{ errors.first(label) }}</div>
+        </div>
+        <button @click="editable=!editable">저장</button>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +45,24 @@ export default {
     value: {
       type: String,
       default: ""
+    },
+    inputType: {
+      type: String,
+      default: "text"
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    inputValidate: {
+      type: String,
+      default: "required"
+    }
+  },
+  methods: {
+    editItem() {
+      this.editable = !this.editable;
+      this.$nextTick(() => this.$refs.itemInput.select());
     }
   }
 };
@@ -56,14 +79,26 @@ export default {
     color: $gray-darker;
   }
 
+  input {
+    display: block;
+    border: 0;
+    width: 100%;
+    font-size: 1.6rem;
+    height: 3.2rem;
+  }
+
   div.value-container {
     height: 4.8rem;
     padding: 0.8rem 0;
+    line-height: 2;
     border-bottom: 1px solid $gray-light;
     display: flex;
     justify-content: space-between;
 
-    button {
+    > span {
+      height: 3.2rem;
+    }
+    > button {
       margin-left: 2.4rem;
       padding: 0 0.8rem;
       color: $gray-dark;
@@ -73,6 +108,22 @@ export default {
         color: $gray-darker;
         border-color: $gray-darker;
         transition: 0.1;
+      }
+    }
+  }
+
+  &.editable {
+    div.value-container {
+      border-color: $gray-dark;
+      transition: 0.1;
+      > button {
+        color: $gray-dark;
+        border-color: $gray-dark;
+        &:hover {
+          color: $gray-darker;
+          border-color: $gray-darker;
+          transition: 0.1;
+        }
       }
     }
   }
